1a36642e29012b16f9288cbdb87402bd624e4b6a
workshimanshh-blip <workshimanshh@gmail.com>
2026-07-14 00:52:26 +0530

    Add Tailwind CSS configuration file

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  darkMode: ["class"],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        border: "hsl(var(--border))",
+        background: "hsl(var(--background))",
+        foreground: "hsl(var(--foreground))",
+      },
+    },
+  },
+  plugins: [],
+}
